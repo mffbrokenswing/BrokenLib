b85a9516d79a4047e9b2663d49fe1c7e671d9a98
Unknown <allocompte@hotmail.fr>
2018-04-30 00:12:50 +0200

    Update version

--- src/main/java/brokenlib/BrokenLib.java
+++ src/main/java/brokenlib/BrokenLib.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class BrokenLib {
 
     public static final String MODID = "brokenlib";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0-SNAPSHOT";
 
     @SidedProxy(clientSide = "brokenlib.client.proxy.ClientProxy", serverSide = "brokenlib.common.proxy.CommonProxy")
     public static CommonProxy proxy;
